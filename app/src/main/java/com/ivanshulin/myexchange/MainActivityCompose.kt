75b26cc4d8fcc3ecb2b5e51c174b76f8d9a26dc8
Ivan Zhulin <ivanzhulin47@gmail.com>
2022-10-30 20:39:27 +0300

    Add theme, manifest

---
+++ app/src/main/java/com/ivanshulin/myexchange/MainActivityCompose.kt
@@ -0,0 +1,15 @@
+package com.ivanshulin.myexchange
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivityCompose: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+        }
+    }
+
+}
